589aca8d2f6664d7ee6c1cb0fd959dff72e490c3
Stéphane Bégaudeau <stephane.begaudeau@gmail.com>
2013-03-25 22:41:49 +0100

    Add support for working sets in the new project wizard.

--- bundles/org.obeonetwork.angularjs.eclipse.tools.ide.ui/src/main/java/org/obeonetwork/angularjs/eclipse/tools/ide/ui/internal/wizards/project/AngularJSProjectDescriptionWizardPage.java
+++ bundles/org.obeonetwork.angularjs.eclipse.tools.ide.ui/src/main/java/org/obeonetwork/angularjs/eclipse/tools/ide/ui/internal/wizards/project/AngularJSProjectDescriptionWizardPage.java
@@ -12,6 +12,7 @@ package org.obeonetwork.angularjs.eclipse.tools.ide.ui.internal.wizards.project;
 
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.dialogs.WizardNewProjectCreationPage;
 import org.obeonetwork.angularjs.eclipse.tools.ide.ui.internal.AngularJSIDEUIMessages;
 import org.obeonetwork.angularjs.eclipse.tools.ide.ui.internal.AngularJSIDEUIPlugin;
@@ -20,6 +21,20 @@ import org.obeonetwork.angularjs.eclipse.tools.ide.ui.internal.AngularJSIDEUIPlu
  * @author <a href="mailto:stephane.begaudeau@obeo.fr">Stephane Begaudeau</a>
  */
 public class AngularJSProjectDescriptionWizardPage extends WizardNewProjectCreationPage {
+	/**
+	 * The "Resource working set" ID.
+	 */
+	protected static final String RESOURCE_WORKING_SET_ID = "org.eclipse.ui.resourceWorkingSetPage"; //$NON-NLS-1$
+
+	/**
+	 * The "Java working set" ID.
+	 */
+	protected static final String JAVA_WORKING_SET_ID = "org.eclipse.jdt.ui.JavaWorkingSetPage"; //$NON-NLS-1$
+
+	/**
+	 * The current selection.
+	 */
+	private IStructuredSelection selection;
 
 	/**
 	 * The constructor.
@@ -32,6 +47,19 @@ public class AngularJSProjectDescriptionWizardPage extends WizardNewProjectCreat
 		this.setTitle(AngularJSIDEUIMessages.getString("AngularJSProjectDescriptionWizardPage.Title")); //$NON-NLS-1$
 		this.setDescription(AngularJSIDEUIMessages
 				.getString("AngularJSProjectDescriptionWizardPage.Description")); //$NON-NLS-1$
+		this.selection = structuredSelection;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.eclipse.ui.dialogs.WizardNewProjectCreationPage#createControl(org.eclipse.swt.widgets.Composite)
+	 */
+	@Override
+	public void createControl(Composite parent) {
+		super.createControl(parent);
+		this.createWorkingSetGroup((Composite)getControl(), this.selection, new String[] {
+				RESOURCE_WORKING_SET_ID, JAVA_WORKING_SET_ID });
 	}
 
 	/**
